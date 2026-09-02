096260af51d20d0e5f3b81b8bdabcf1f97f8bfe6
idanwais <idanwais@gmail.com>
2026-09-02 12:11:02 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,31 @@
-const CACHE='shir-singlefile-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+const CACHE='shir-singlefile-v2';
+const ASSETS=['./','./index.html'];
+
+self.addEventListener('install',e=>{
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c=>c.addAll(ASSETS))
+      .then(()=>self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch',e=>{
- if(e.request.method!=='GET')return;
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const clone=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,clone));return resp;})));
+  if(e.request.method!=='GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(resp=>{
+        const clone=resp.clone();
+        caches.open(CACHE).then(c=>c.put(e.request,clone));
+        return resp;
+      })
+      .catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html')))
+  );
 });
